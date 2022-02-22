c750c8a2fd806fc95c423c0657c0063fe81443bc
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-02-22 09:54:12 -0600

    simplify modal styles

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,27 +12,23 @@ const Modal = ({ isOpen, setIsOpen, title }: ModalProps) => (
   <ReactModal
     ariaHideApp={false}
     isOpen={isOpen}
-    overlayClassName="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-    className="flex min-h-screen justify-center px-4 pt-4 pb-20"
+    overlayClassName="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity flex flex-col justify-center min-h-screen"
+    className="mx-auto max-w-md font-mono text-sm text-white focus:outline-0"
     onRequestClose={() => setIsOpen(false)}
   >
-    <div className="flex flex-col justify-center">
-      <div className="max-w-md font-mono text-sm text-white">
-        <div className="flex items-center justify-end bg-purple-600 px-4 py-2">
-          <div className="flex-grow leading-none">{title}</div>
-          <div
-            className="cursor-pointer font-mono"
-            onClick={() => setIsOpen(false)}
-          >
-            <XIcon />
-          </div>
-        </div>
-
-        <div className="bg-purple-500 p-4">
-          Hello, this is the modal. What happens if the text gets really long, I
-          wonder?
-        </div>
+    <div className="flex items-center justify-end bg-purple-600 px-4 py-2">
+      <div className="flex-grow leading-none">{title}</div>
+      <div
+        className="cursor-pointer font-mono"
+        onClick={() => setIsOpen(false)}
+      >
+        <XIcon />
       </div>
+    </div>
+
+    <div className="bg-purple-500 p-4">
+      Hello, this is the modal. What happens if the text gets really long, I
+      wonder?
     </div>
   </ReactModal>
 );
