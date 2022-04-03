bd582f928853ca89e0763e2e0078b8ab8f932ba9
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-04-03 08:15:57 -0500

    remove config options that are seemingly ignored

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("@prisma/client");
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
